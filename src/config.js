840289062002fc5a4c0fb081b6a7f0c5d6414277
retroverse <ebreakey@gmail.com>
2020-01-27 20:18:00 +1100

    Add serve script

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
-dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 export const BACK_END_AUTH_ADDRESS = process.env.BACK_END_AUTH_ADDRESS || 'http://localhost:8090/auth'
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
