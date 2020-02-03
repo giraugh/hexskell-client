139c490265358f2190d5114df0f772d5107eb64d
retroverse <ebreakey@gmail.com>
2020-02-03 16:11:50 +1100

    Redirect to dashboard on login and home on logout

--- src/components/AccountAction.js
+++ src/components/AccountAction.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLoggedIn } from '../hooks/authentication'
 
 import SignIn from './SignIn'
 import AccountDropDown from './AccountDropDown'
+import { Redirect } from 'react-router-dom'
 
 const AccountAction = () => {
   const { loggedIn, logout, login } = useLoggedIn()
+  const [redirectTo, setRedirectTo] = useState(null)
+
+  const handleLogin = () => {
+    console.log('loginnnn')
+    login()
+    setRedirectTo('/dash')
+  }
+
+  const handleLogout = () => {
+    logout()
+    setRedirectTo('/')
+    console.log('just logged out')
+  }
 
   return <div className='account'>
-    {
-      loggedIn ? <AccountDropDown logoutCallback={logout} /> : <SignIn callback={login} />
-    }
+    <>
+      { redirectTo && <Redirect to={redirectTo} /> }
+      { loggedIn ? <AccountDropDown logoutCallback={handleLogout} /> : <SignIn callback={handleLogin} /> }
+    </>
   </div>
 }
 
