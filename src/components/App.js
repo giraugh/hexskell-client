31fd31ff7d09f82ecb18aa3e810092722612993a
retroverse <ebreakey@gmail.com>
2020-02-05 20:48:26 +1100

    Add dependency to scroll page after route change

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Segment, Container } from 'semantic-ui-react'
+import ScrollToTop from 'react-router-scroll-top'
 
 import TopBar from './TopBar'
 
@@ -18,22 +19,24 @@ const withSuspense = (Page, props) => _ => (<Suspense fallback={<div>loading...<
 const App = () => {
   return (
     <Router>
-      <Segment basic style={{ minHeight: 700, padding: '1em 0em' }}>
-        <TopBar />
-        <Container>
-          <Switch>
-            <Route exact path='/' component={withSuspense(HomePage)} />
-            <Route exact path='/bots' component={withSuspense(BotsPage)}/>
-            <Route exact path='/bot/:id' component={withSuspense(BotPage)}/>
-            <Route exact path='/user/:id' component={withSuspense(UserPage)}/>
-            <Route exact path='/settings' component={withSuspense(SettingsPage)}/>
-            <Route exact path='/dash' component={withSuspense(DashboardPage)}/>
-            <Route exact path='/create-bot' component={withSuspense(CreateBotPage)}/>
-            <Route exact path='/create-bot/continue/:id' component={withSuspense(EditBotPage, { isContinue: true })}/>
-            <Route exact path='/edit-bot/:id' component={withSuspense(EditBotPage)}/>
-          </Switch>
-        </Container>
-      </Segment>
+      <ScrollToTop>
+        <Segment basic style={{ minHeight: 700, padding: '1em 0em' }}>
+          <TopBar />
+          <Container>
+            <Switch>
+              <Route exact path='/' component={withSuspense(HomePage)} />
+              <Route exact path='/bots' component={withSuspense(BotsPage)}/>
+              <Route exact path='/bot/:id' component={withSuspense(BotPage)}/>
+              <Route exact path='/user/:id' component={withSuspense(UserPage)}/>
+              <Route exact path='/settings' component={withSuspense(SettingsPage)}/>
+              <Route exact path='/dash' component={withSuspense(DashboardPage)}/>
+              <Route exact path='/create-bot' component={withSuspense(CreateBotPage)}/>
+              <Route exact path='/create-bot/continue/:id' component={withSuspense(EditBotPage, { isContinue: true })}/>
+              <Route exact path='/edit-bot/:id' component={withSuspense(EditBotPage)}/>
+            </Switch>
+          </Container>
+        </Segment>
+      </ScrollToTop>
     </Router>
   )
 }
